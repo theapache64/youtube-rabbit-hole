0d38bcc367000025b25c7b8579723f7deb7eb27e
theapache64 <theapache64@gmail.com>
2020-11-28 09:44:56 +0530

    🌛 first commit

---
+++ app/src/main/java/com/theapache64/youtube/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.theapache64.youtube
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private val usefulApps = listOf(
+                "com.quora.android", // Quora
+                "com.google.android.apps.books", // Google Books
+                // add as many good apps here
+        )
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val packageName = usefulApps.random()
+        var appIntent = packageManager.getLaunchIntentForPackage(packageName)
+
+        if (appIntent == null) {
+            // app not installed, so let's redirect to play store
+            appIntent = Intent(Intent.ACTION_VIEW).apply {
+                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                data = Uri.parse("market://details?id=$packageName")
+            }
+        }
+
+        startActivity(appIntent)
+        finish()
+    }
+}
